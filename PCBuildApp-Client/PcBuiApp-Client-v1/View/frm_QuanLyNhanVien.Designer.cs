16e24890aee6343c4c4e7e6dfe06e72e5d0082e6
Ho My Kim <homykim26729@gmail.com>
2024-10-08 22:27:13 +0700

    Test Commit On Branch HMK

--- PCBuildApp-Client/PcBuiApp-Client-v1/View/frm_QuanLyNhanVien.Designer.cs
+++ PCBuildApp-Client/PcBuiApp-Client-v1/View/frm_QuanLyNhanVien.Designer.cs
@@ -28,21 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_QuanLyNhanVien));
             btn_CloseLogin = new Button();
             pictureBox1 = new PictureBox();
             roundedUserControl1 = new UserControlCustom.RoundedUserControl();
-            roundedUserControl2 = new UserControlCustom.RoundedUserControl();
-            pictureBox2 = new PictureBox();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             label1 = new Label();
+            pictureBox2 = new PictureBox();
+            panel3 = new Panel();
+            roundedUserControl2 = new UserControlCustom.RoundedUserControl();
+            label2 = new Label();
+            roundedUserControl3 = new UserControlCustom.RoundedUserControl();
+            textBox1 = new TextBox();
+            roundedUserControl4 = new UserControlCustom.RoundedUserControl();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            Column_ListAccountSTT = new DataGridViewTextBoxColumn();
+            Column_ListAccountName = new DataGridViewTextBoxColumn();
+            Column_ListAccountAction = new DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             roundedUserControl1.SuspendLayout();
-            roundedUserControl2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            roundedUserControl2.SuspendLayout();
+            roundedUserControl3.SuspendLayout();
+            roundedUserControl4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // btn_CloseLogin
             // 
-            btn_CloseLogin.Location = new Point(1337, 12);
+            btn_CloseLogin.Location = new Point(1367, 12);
             btn_CloseLogin.Margin = new Padding(4, 3, 4, 3);
             btn_CloseLogin.Name = "btn_CloseLogin";
             btn_CloseLogin.Size = new Size(34, 27);
@@ -65,7 +86,14 @@
             // roundedUserControl1
             // 
             roundedUserControl1.BackColor = Color.White;
-            roundedUserControl1.Controls.Add(roundedUserControl2);
+            roundedUserControl1.Controls.Add(pictureBox2);
+            roundedUserControl1.Controls.Add(label1);
+            roundedUserControl1.Controls.Add(button6);
+            roundedUserControl1.Controls.Add(button5);
+            roundedUserControl1.Controls.Add(button4);
+            roundedUserControl1.Controls.Add(button3);
+            roundedUserControl1.Controls.Add(button2);
+            roundedUserControl1.Controls.Add(button1);
             roundedUserControl1.Controls.Add(pictureBox1);
             roundedUserControl1.CornerRadius = 50;
             roundedUserControl1.Location = new Point(12, 12);
@@ -73,42 +101,223 @@
             roundedUserControl1.Size = new Size(345, 836);
             roundedUserControl1.TabIndex = 15;
             // 
-            // roundedUserControl2
+            // button6
             // 
-            roundedUserControl2.Controls.Add(pictureBox2);
-            roundedUserControl2.Controls.Add(label1);
-            roundedUserControl2.CornerRadius = 20;
-            roundedUserControl2.Location = new Point(18, 216);
-            roundedUserControl2.Name = "roundedUserControl2";
-            roundedUserControl2.Size = new Size(302, 63);
-            roundedUserControl2.TabIndex = 1;
+            button6.BackColor = SystemColors.ButtonFace;
+            button6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.ImageAlign = ContentAlignment.MiddleLeft;
+            button6.Location = new Point(21, 675);
+            button6.Name = "button6";
+            button6.Padding = new Padding(20, 0, 0, 0);
+            button6.Size = new Size(296, 41);
+            button6.TabIndex = 16;
+            button6.Text = "Quản Lý Tài Khoản";
+            button6.UseVisualStyleBackColor = false;
             // 
-            // pictureBox2
+            // button5
             // 
-            pictureBox2.Image = Properties.Resources._1000_F_272554194_9bJhYSJkUcLbTjXfDlDANV2RyQYGO06N;
-            pictureBox2.Location = new Point(13, 14);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(42, 29);
-            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
+            button5.BackColor = SystemColors.ButtonFace;
+            button5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(21, 595);
+            button5.Name = "button5";
+            button5.Padding = new Padding(20, 0, 0, 0);
+            button5.Size = new Size(296, 41);
+            button5.TabIndex = 5;
+            button5.Text = "Quản Lý Tài Khoản";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ButtonFace;
+            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(21, 514);
+            button4.Name = "button4";
+            button4.Padding = new Padding(20, 0, 0, 0);
+            button4.Size = new Size(296, 41);
+            button4.TabIndex = 4;
+            button4.Text = "Quản Lý Tài Khoản";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonFace;
+            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(21, 431);
+            button3.Name = "button3";
+            button3.Padding = new Padding(20, 0, 0, 0);
+            button3.Size = new Size(296, 41);
+            button3.TabIndex = 3;
+            button3.Text = "Quản Lý Tài Khoản";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonFace;
+            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(21, 355);
+            button2.Name = "button2";
+            button2.Padding = new Padding(20, 0, 0, 0);
+            button2.Size = new Size(296, 41);
+            button2.TabIndex = 2;
+            button2.Text = "Quản Lý Tài Khoản";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(21, 276);
+            button1.Name = "button1";
+            button1.Padding = new Padding(20, 0, 0, 0);
+            button1.Size = new Size(296, 41);
+            button1.TabIndex = 1;
+            button1.Text = "Quản Lý Tài Khoản";
+            button1.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(64, 20);
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.AppWorkspace;
+            label1.Location = new Point(115, 193);
             label1.Name = "label1";
-            label1.Size = new Size(198, 19);
-            label1.TabIndex = 0;
-            label1.Text = "Quản Lý Tài Khoản Nhân Viên";
+            label1.Size = new Size(143, 17);
+            label1.TabIndex = 1;
+            label1.Text = "homykim@gmail.com";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(21, 174);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(84, 66);
+            pictureBox2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(roundedUserControl2);
+            panel3.Location = new Point(383, 96);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1016, 66);
+            panel3.TabIndex = 17;
+            // 
+            // roundedUserControl2
+            // 
+            roundedUserControl2.BackColor = Color.White;
+            roundedUserControl2.Controls.Add(label2);
+            roundedUserControl2.CornerRadius = 20;
+            roundedUserControl2.Location = new Point(0, 4);
+            roundedUserControl2.Name = "roundedUserControl2";
+            roundedUserControl2.Size = new Size(1016, 66);
+            roundedUserControl2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ActiveCaption;
+            label2.Location = new Point(12, 20);
+            label2.Name = "label2";
+            label2.Size = new Size(332, 21);
+            label2.TabIndex = 0;
+            label2.Text = "Quản Lý Tài Khoản \\ Danh Sách Nhân Viên";
+            // 
+            // roundedUserControl3
+            // 
+            roundedUserControl3.BackColor = Color.White;
+            roundedUserControl3.Controls.Add(textBox1);
+            roundedUserControl3.CornerRadius = 20;
+            roundedUserControl3.Location = new Point(383, 27);
+            roundedUserControl3.Name = "roundedUserControl3";
+            roundedUserControl3.Size = new Size(344, 50);
+            roundedUserControl3.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.White;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(3, 14);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Tìm kiếm ...";
+            textBox1.Size = new Size(341, 22);
+            textBox1.TabIndex = 0;
+            // 
+            // roundedUserControl4
+            // 
+            roundedUserControl4.BackColor = Color.White;
+            roundedUserControl4.Controls.Add(label3);
+            roundedUserControl4.Controls.Add(dataGridView1);
+            roundedUserControl4.CornerRadius = 50;
+            roundedUserControl4.Location = new Point(383, 181);
+            roundedUserControl4.Name = "roundedUserControl4";
+            roundedUserControl4.Size = new Size(1018, 667);
+            roundedUserControl4.TabIndex = 19;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ActiveCaption;
+            label3.Location = new Point(12, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(176, 21);
+            label3.TabIndex = 1;
+            label3.Text = "Danh Sách Nhân Viên";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column_ListAccountSTT, Column_ListAccountName, Column_ListAccountAction });
+            dataGridView1.Location = new Point(3, 93);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(1012, 574);
+            dataGridView1.TabIndex = 0;
+            // 
+            // Column_ListAccountSTT
+            // 
+            Column_ListAccountSTT.FillWeight = 20F;
+            Column_ListAccountSTT.HeaderText = "STT";
+            Column_ListAccountSTT.Name = "Column_ListAccountSTT";
+            // 
+            // Column_ListAccountName
+            // 
+            Column_ListAccountName.HeaderText = "Tên Tài Khoản";
+            Column_ListAccountName.Name = "Column_ListAccountName";
+            // 
+            // Column_ListAccountAction
+            // 
+            Column_ListAccountAction.HeaderText = "Hành Động";
+            Column_ListAccountAction.Name = "Column_ListAccountAction";
             // 
             // frm_QuanLyNhanVien
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonFace;
-            ClientSize = new Size(1384, 869);
+            ClientSize = new Size(1411, 869);
+            Controls.Add(roundedUserControl4);
+            Controls.Add(roundedUserControl3);
+            Controls.Add(panel3);
             Controls.Add(roundedUserControl1);
             Controls.Add(btn_CloseLogin);
             FormBorderStyle = FormBorderStyle.None;
@@ -118,9 +327,16 @@
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             roundedUserControl1.ResumeLayout(false);
+            roundedUserControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel3.ResumeLayout(false);
             roundedUserControl2.ResumeLayout(false);
             roundedUserControl2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            roundedUserControl3.ResumeLayout(false);
+            roundedUserControl3.PerformLayout();
+            roundedUserControl4.ResumeLayout(false);
+            roundedUserControl4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
@@ -129,8 +345,24 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn_CloseLogin;
         private UserControlCustom.RoundedUserControl roundedUserControl1;
-        private UserControlCustom.RoundedUserControl roundedUserControl2;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
         private PictureBox pictureBox2;
         private Label label1;
+        private Panel panel3;
+        private UserControlCustom.RoundedUserControl roundedUserControl2;
+        private UserControlCustom.RoundedUserControl roundedUserControl3;
+        private Label label2;
+        private TextBox textBox1;
+        private UserControlCustom.RoundedUserControl roundedUserControl4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column_ListAccountSTT;
+        private DataGridViewTextBoxColumn Column_ListAccountName;
+        private DataGridViewTextBoxColumn Column_ListAccountAction;
+        private Label label3;
     }
 }
